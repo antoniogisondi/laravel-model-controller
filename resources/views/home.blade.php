75ef94af2e36f33b90a010bc36eb10d974608ab4
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-26 19:04:34 +0200

    Bonus done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,9 @@
                 <div class="col">
                     <a href="{{ route('details', $comic->id) }}">
                         <div class="card mt-3">
-                            <img src="{{ $comic->thumb}}" alt="">
+                            <img src="{{ $comic->thumb}}">
                             <div class="card-body">
                                 <h4>{{ $comic->title }}</h4>
-                                <p>{{ $comic->description }}</p>
-                                <p>Price: {{ $comic->price }}</p>
-                                <p>Series: {{ $comic->series }}</p>
-                                <p>Type: {{ $comic->type }}</p>
-                                <p>Date: {{ $comic->sale_date }}</p>
-                                <p>Artists: {{ $comic->artists }}</p>
-                                <p>Writers: {{ $comic->writers }}</p>
                             </div>
                         </div>
                     </a>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,22 @@
         <div class="row">
             <div class="col">
                 <div class="card">
-                    
+                    <div class="card mt-3 d-flex flex-row">
+                        <div class="card-img-top d-flex justify-content-center my-3">
+                            <img src="{{ $comics->thumb}}">
+                        </div>
+                        
+                        <div class="card-body">
+                            <h4>{{ $comics->title }}</h4>
+                            <p>{{ $comics->description }}</p>
+                            <p>Price: {{ $comics->price }}</p>
+                            <p>Series: {{ $comics->series }}</p>
+                            <p>Type: {{ $comics->type }}</p>
+                            <p>Date: {{ $comics->sale_date }}</p>
+                            <p>Artists: {{ $comics->artists }}</p>
+                            <p>Writers: {{ $comics->writers }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
